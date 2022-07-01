9ea0449ebfb43434d7ccc129cb42401c832aaf09
Chandler Davis <echandlerdavis@gmail.com>
2022-07-01 14:11:18 -0700

    fixed small but with /products and /tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,19 +34,42 @@ router.get('/:id', async (req, res) => {
 });
 
 //CREATE a category
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 //UPDATE a category
-router.put('/:id', (req, res) => {
-
+// router.put('/:id', async (req, res) => {
+//   try{
+//     const categoryData = await Category.update({req.body}, );
+//     res.status(200).json(categoryData);
+//   }
   // update a category by its `id` value
-});
+// });
 
 //DELETE a category
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    if(!categoryData) {
+      res.status(404).json({ message: "No category found with this id!"});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,82 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+
+  //include: [{model:Product, as: "product"}]
+}
   // find all tags
   // be sure to include its associated Product data
-});
+);
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try{
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, as: 'product'}]
+    });
+    if(!tagData){
+      res.status(404).json({ message: 'No tag found with this id!'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+try {
+  const tagData = await Tag.update(req.body,
+    {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(tagData)
+} catch (err){
+  res.status(500).json(err);
+}l
+  //look at exercise 10 in mvc week! hints baby! I feel like there's more to this.. but i'm not totally sure. just try it out
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    if(!tagData) {
+      res.status(404).json({ message: "No tag found with this id!"});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
